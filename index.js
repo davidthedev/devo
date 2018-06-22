3965c9c5a9f9cefc761bb8ad62badcc1074cf8db
David C <davidthedev@users.noreply.github.com>
2018-06-22 22:20:25 +0100

    Add messaging functionality

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const routes = require('./app/routes');
 
-io.on('connection', function (socket) {
-  console.log('a user connected');
-});
-
 app.use(express.static('public'))
 
 routes(app);
+
+io.on('connection', function (socket) {
+  socket.on('chat message', function (msg) {
+    io.emit('chat message', msg);
+  });
+});
 
 server.listen(8080);
