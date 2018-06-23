71a6edb6c25b27d96d13369207c290d1e98067bd
David C <davidthedev@users.noreply.github.com>
2018-06-23 22:10:19 +0100

    Add new routes

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,25 @@
+const path = require('path');
+
 module.exports = (app) => {
   app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/../public/index.html');
+    res.sendFile(path.resolve('public/index.html'));
+  });
+
+  app.get('/login', (req, res) => {
+    res.sendFile(path.resolve('public/login.html'));
+  });
+
+  app.post('/login', (req, res) => {
+    // TODO
+    // save req.body params (name, email, query?) to db
+    // on succcess, send redirect url
+    res.status(200).json({ urlSegment: '/chat' });
+
+    // TODO (failed response)
+    // res.status(500).json({ msg: 'failed' });
+  });
+
+  app.get('/chat', (req, res) => {
+    res.sendFile(path.resolve('public/index.html'));
   });
 };
