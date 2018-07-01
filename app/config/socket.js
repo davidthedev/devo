7d82b568f174435e2505d01dc0649775ce6fcfdd
David C <davidthedev@users.noreply.github.com>
2018-07-01 13:28:49 +0100

    Add socket config

---
+++ app/config/socket.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "message": "message",
+  "joinAsUser": "join-as-user",
+  "joinedAsUser": "joined-as-user",
+  "joinAsOp": "join-as-op",
+  "joinedAsOp": "joined-as-op",
+  "pendingOpMsg": "Waiting for the next available agent",
+  "joinedAsOpMsg": "{op_name} joined the chat"
+}
