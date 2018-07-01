94c90bd4b0ccfa6ed008c97e29f62b662d9d07b1
David C <davidthedev@users.noreply.github.com>
2018-07-01 13:27:10 +0100

    Add comments and update socket functionality

--- app/helpers/socket-io.js
+++ app/helpers/socket-io.js
@@ -1,21 +1,48 @@
 const io = require('socket.io');
+const socketConfig = require('../config/socket');
 
 module.exports = {
-  run(server) {
+  connect(server) {
     const sockets = new io(server);
-    const ioNsp = sockets.of('/chat'); // namespace
+    const ioNsp = sockets.of('/chat'); // socket namespace
 
     ioNsp.on('connection', (socket) => {
-      socket.on('join', (room) => {
-        socket.join(room);
+      /*
+       * Join a room, inform the operators
+       */
+      socket.on(socketConfig.joinAsUser, (params, cb) => {
+        // join room
+        socket.join(params.roomId);
+        // inform operator that a new user just joined an online chat
+        sockets.of('/panel').emit(socketConfig.joinedAsUser, params.roomId);
+        // send confirmation back to user
+        cb(socketConfig.pendingOpMsg);
       });
 
-      socket.on('message', (msg, room) => {
-        ioNsp.in(room).emit('message', msg);
+      /*
+       * Join a room, as an operator
+       */
+      socket.on(socketConfig.joinAsOp, params => {
+        socket.join(params.roomId);
+
+        const joinedAsOpMsg = socketConfig.joinedAsOpMsg.replace('{op_name}', params.opName);
+
+        ioNsp.in(params.roomId).emit(socketConfig.joinedAsOp, joinedAsOpMsg);
       });
 
-      socket.on('user-joined', (room) => {
-        sockets.of('/panel').emit('user-joined', room);
+      /*
+       * Emit received message
+       */
+      socket.on(socketConfig.message, (params) => {
+        ioNsp.in(params.roomId).emit(socketConfig.message, params.msg);
+      });
+
+      /*
+       * Inform operator that a new user joined support chat
+       */
+      socket.on('socketConfig.joinedAsUser', (room) => {
+        sockets.of('/panel').emit('socketConfig.joinedAsUser', room);
+        ioNsp.in(room).emit('')
       })
     });
   }
