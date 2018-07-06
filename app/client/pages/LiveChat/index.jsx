e03235e29426d6700f002009aa7d3724c67e41f7
David C <davidthedev@users.noreply.github.com>
2018-07-06 22:44:12 +0100

    Wrap LiveChat component

--- app/client/pages/LiveChat/index.jsx
+++ app/client/pages/LiveChat/index.jsx
@@ -3,13 +3,17 @@ import React from 'react';
 export default () => {
   return (
     <div>
-      <h2>Your chat</h2>
-      <form>
-        <input type="text" name="name" id="name" placeholder="Your name" />
-        <input type="text" name="email" id="email" placeholder="Your email" />
-        <input type="text" name="question" id="email" placeholder="Your question" />
-        <input type="submit" name="message_btn" value="Send" />
-      </form>
+      <div className="lc-c-head">
+        <h2>Get in touch</h2>
+      </div>
+      <div>
+        <form>
+          <input type="text" name="name" id="name" placeholder="Your name" />
+          <input type="text" name="email" id="email" placeholder="Your email" />
+          <input type="text" name="question" id="email" placeholder="Your question" />
+          <input type="submit" name="message_btn" value="Send" />
+        </form>
+      </div>
     </div>
   )
 }
