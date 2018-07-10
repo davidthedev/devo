69304f7880953fa3d454da6f314b2e16d9541aa4
David C <davidthedev@users.noreply.github.com>
2018-07-10 23:08:35 +0100

    Start moving logic to react component

--- app/client/pages/LiveChat/index.jsx
+++ app/client/pages/LiveChat/index.jsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-  return (
-    <div>
-      <div className="lc-c-head">
-        <h2>Get in touch</h2>
-      </div>
+export default class LiveChat extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    console.log(e.target);
+    e.preventDefault();
+  }
+
+  render() {
+    return (
       <div>
-        <form>
-          <input type="text" name="name" id="name" placeholder="Your name" />
-          <input type="text" name="email" id="email" placeholder="Your email" />
-          <input type="text" name="question" id="email" placeholder="Your question" />
-          <input type="submit" name="message_btn" value="Send" />
-        </form>
+        <div className="lc-c-head">
+          <h2>Get in touch</h2>
+        </div>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" name="name" id="name" placeholder="Your name" />
+            <input type="text" name="email" id="email" placeholder="Your email" />
+            <input type="text" name="question" id="email" placeholder="Your question" />
+            <input type="submit" name="message_btn" />
+          </form>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
